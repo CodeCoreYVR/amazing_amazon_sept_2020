afd58a6cdea3c3bc701804c0f25fe71139ffd6f1
Naveed Raza <naveedraza@gmail.com>
2020-08-06 15:05:32 -0700

    Amazon API: Product Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
-  get('/home', to: 'welcome#home')
-  get('/about', to: 'welcome#about')
-  get('/contact_us', to: 'welcome#contact_us')
-  post('/thank_you', to: 'welcome#thank_you')
+  root "welcome#home"
+  get("/home", to: "welcome#home")
+  get("/about", to: "welcome#about")
+  get("/contact_us", to: "welcome#contact_us")
+  post("/thank_you", to: "welcome#thank_you")
 
   #bill splitter
-  get '/bill_splitter', to: 'bill_splitter#new'
-  post '/calculate_split', to: 'bill_splitter#create'
+  get "/bill_splitter", to: "bill_splitter#new"
+  post "/calculate_split", to: "bill_splitter#create"
 
   # Session Routes
   resource :session, only: [:new, :create, :destroy]
@@ -39,4 +39,24 @@ Rails.application.routes.draw do
   end
 
   resources :news_articles
+
+  # API routes
+
+  # The namespace method in Rails routes makes it so that
+  # your app will automatically look in a directory api,
+  # then in a sub directory v1 for QuestionsController
+
+  # The option 'defaults: { format: :json }' will set 'json' as
+  # the default response format for all routes contained within
+  # the block.
+
+  # The option 'defaults: { format: :json }' will set 'json' as
+  # the default response format for all routes contained within
+  # the block.
+
+  namespace :api, defaults: { format: :json } do #👈🏻 we can set default response format of the block
+    namespace :v1 do
+      resources :products, only:[:index]
+    end
+  end
 end
