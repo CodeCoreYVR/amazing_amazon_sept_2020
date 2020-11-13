193f0c58187559010f9a72809cb778139c424499
Anson Lam <anson.lam94@gmail.com>
2020-11-13 14:17:08 -0800

    [Lab] Product Model Callbacks

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  # A contant is a value that shouldn't change in our program. It is a hard coded value. 
+  # If we ever need change the default price, we can update it here at the top.
+  # Anywhere that we use this constant, it will refer to its value.
+  # By convention it's SCREAMING_SNAKE_CASE
+  DEFAULT_PRICE = 1 
+
   # To generate a model, run:
   # > rails g model <singular-model-name> <...column-name:column-type...>
   # When running this, a migration is also generated for us under db/migrate
@@ -12,6 +18,20 @@ class Product < ApplicationRecord
   # We also have class methods such as Product.all which will return 
   # an ActiveRecord Relation of all product instances saved to our db
 
+  # An object/instance goes through different stages in its lifecycle
+  # Callbacks allow us to to do something at a point in the object's life by running a method
+  # Examples:
+  # 1. Initialzing an object using .new(): after_initialize
+  # 2. Creating an object: before_validation, after_validation, before_save, before_create, after_create, after_save
+  # 3. Updating an object: before_validation, after_validation, before_save, before_update, after_update, after_save
+  # 4. Destroying an object: before_destroy, after_destroy
+
+  # We don't want price to be nil before we go through the validations
+  # The lifecycle method takes a symbol named after the method
+  # which it will call at that stage of the object's life
+  before_validation :set_default_price
+  before_save :capitalize_title
+
   # Create validations using the "validates" method
   # The arguments to the method are: 
   # 1. The column name as a symbol
@@ -21,4 +41,22 @@ class Product < ApplicationRecord
   validates :title, presence: true, uniqueness: { case_sensitive: false }
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true, length: { minimum: 10 }
+
+  private
+
+  # It's best practice to declare our custom callback methods as private, if left public then 
+  # it violates object encapsulation principle.
+  def set_default_price
+    # If you are setting, you must prefix with self. 
+    # It's optional to prefix with self if you're just getting.
+    # e.g. price -> the instance's price
+
+    self.price ||= DEFAULT_PRICE
+  end
+
+  def capitalize_title
+    # capitalize! mutates the string it was called on, in this case, the title in the object
+    # self.title ||= self.title.capitalize
+    self.title.capitalize!
+  end
 end
