42fbc562c17ca3ec4798ab531029135171e187b9
Anson Lam <anson.lam94@gmail.com>
2020-11-13 11:05:11 -0800

    [Lab] Product Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all()
+
+1000.times do
+  created_at = Faker::Date.backward(days: 365 * 5)
+
+  Product.create({
+    title: Faker::Commerce.product_name,
+    description: Faker::Hacker.say_something_smart,
+    price: Faker::Commerce.price(range: 0..10_000.00),
+    created_at: created_at,
+    updated_at: created_at,
+  })
+end
+
+puts Cowsay.say("Created #{Product.count} products", :dragon)
